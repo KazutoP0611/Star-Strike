f9a03d54eb69bbfc6aeb1c93e8839b54a50c031d
PPYukito <PanutatR@gmail.com>
2026-05-13 15:58:45 +0900

    [Mod] Added shooting sound, modified bullet speed;

--- Assets/Scripts/Entities/Entity_Bullet.cs
+++ Assets/Scripts/Entities/Entity_Bullet.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class Entity_Bullet : MonoBehaviour
 {
+    private float destroyTime;
+
+    [Header("General Details")]
     [SerializeField] private float bulletSpeed = 30.0f;
+    [SerializeField] private float destroyInSecs = 3.0f;
+
+    private void OnEnable()
+    {
+        destroyTime = Time.time + destroyInSecs;
+    }
 
     private void Update()
     {
         transform.position += transform.forward * Time.deltaTime * bulletSpeed;
+
+        if (Time.time >= destroyTime)
+            Destroy(gameObject);
     }
 }
--- Assets/Scripts/Players/PlayerWeapon.cs
+++ Assets/Scripts/Players/PlayerWeapon.cs
@@ -15,6 +15,7 @@ public class PlayerWeapon : MonoBehaviour
 
     [Header("General Details")]
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private AudioSource audioSource;
     [SerializeField] private float shootInterval;
 
     [Header("Shooting Transform Details")]
@@ -62,5 +63,6 @@ public class PlayerWeapon : MonoBehaviour
     private void Shoot(Transform shootPoint)
     {
         Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+        audioSource.Play(0);
     }
 }
